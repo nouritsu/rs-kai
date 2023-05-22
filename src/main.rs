6f96eb55f72e13da5de013fbe7dfa62b194d6939
root <root@Aneesh-PC.localdomain>
2023-05-22 13:50:56 +0530

    implemented cd inbuilt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::{
+    env,
+    io::{stdin, stdout, Write},
+    path::Path,
+    process::Command,
+};
+
+#[allow(unused_must_use)]
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        print!("kai :> ");
+        stdout().flush();
+
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
+
+        let mut parts = input.trim().split_whitespace();
+        let command = parts.next().unwrap();
+        let args = parts;
+
+        match command {
+            "cd" => {
+                let next_dir = args.peekable().peek().map_or("/", |x| *x);
+                let root = Path::new(next_dir);
+                if let Err(e) = env::set_current_dir(&root) {
+                    eprintln!("{}", e);
+                }
+            }
+            command => {
+                let mut child = Command::new(command).args(args).spawn().unwrap();
+                child.wait();
+            }
+        }
+    }
 }
